fa9178864dbfcbce8f4e9d475dcf3561331a1cb8
Sktbanerjee1 <saikat.10.05.95@gmail.com>
2023-08-18 11:26:53 -0400

    Script added for running FASTQC

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # Diversity Outbred (DO) bulk RNA-Seq pipeline
 
-## Overview of the pipeline
+## Getting started
+
+Clone this repository in your computing environment
+
+```bash
+#!/usr/bin/bash
+# make sure git is set up to handle SSH login, alternatively use HTTPS
+git clone git@github.com:Sktbanerjee1/Diversity_outbred_bulk_RNA-Seq.git
+```
+
+## Workflow for the RNA-Seq Analysis
+
+### QC of the FASTQ files
+
+assumed execution from the project folder path `.../Diversity_outbred_bulk_RNA-Seq/`
+
+Run FATQC on local computing environment
+
+```bash
+#!/bin/bash
+# make sure FASTQC is installed and availble in the path
+bash src/sh/run_fastqc.sh --help
+```
+Usage: src/sh/run_fastqc.sh -i target_dir -o out_dir -m run_mode
+    -i path to directory with fastq files
+    -o Path to the directory where the outputs will be written
+    -m value should be one of Trimmed/Untrimmed 
+
+Run FASTQC on Rivanna (UVA internal)   
+```bash
+#!/bin/bash
+# running with slurm (UVA internal)
+sbatch src/slurm/run_fastqc.slurm
+```
+In this case the `.slurm` script needs to be edited. The user should change the values of  `target_dir`, `out_dir`, and `run_mode` in the slurm script.
---
+++ src/sh/run_fastqc.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -i target_dir -o out_dir -m run_mode"
+   echo -e "\t-i path to directory with fastq files"
+   echo -e "\t-o Path to the directory where the outputs will be written"
+   echo -e "\t-m value should be one of Trimmed/Untrimmed "
+   exit 1 # Exit script after printing help
+}
+
+while getopts "i:o:m:" opt
+do
+   case "$opt" in
+      i ) target_dir="$OPTARG" ;;
+      o ) out_dir="$OPTARG" ;;
+      m ) run_mode="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$target_dir" ] || [ -z "$out_dir" ] || [ -z "$run_mode" ]
+then
+   echo "All the parameters are required!";
+   helpFunction
+fi
+
+
+# Begin script in case all parameters are correct
+now=$(date)
+echo "_________________________"
+echo "${now}"
+target_dir=$(realpath ${target_dir})
+echo "target: ${target_dir}"
+echo "_________________________"
+
+# move to out directory
+echo "out_dir: ${out_dir}"
+mkdir -p ${out_dir}
+out_dir=$(realpath ${out_dir})
+cd ${out_dir}
+
+# find fastq files
+if [[ $run_mode == "Trimmed" ]]; then
+  # Trimmed mode
+  echo "QC statered in Trimmed Mode"
+  # iterate over each dir
+  files=$(find ${target_dir} | grep -i "_paired.fastq.gz")
+  # end Trimmed mode
+elif [[ $run_mode == "Untrimmed" ]]; then
+  # Untrimmed mode
+  echo "QC statered in Untrimmed Mode"
+  echo "Scanning for all .fastq files"
+  files=$(ls ${target_dir}/*.fastq.gz)
+  # end Untrimmed mode
+else
+  echo "Invalid Run Mode Specified"
+fi
+
+# get files
+files=( ${files} )
+echo "found:${#files[@]} files"
+
+# process files
+for f in ${files[@]}
+do
+echo "Processing: ${f}"
+fastqc -t 10 $(realpath ${f}) -o .
+done
+
+echo "Done!"
